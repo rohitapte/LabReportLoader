4b085e383a4090e211fdf0efd8f6f7ea25c948b2
Rohit Apte <rohitapte@gmail.com>
2018-09-16 18:45:03 +0800

    empty test class

--- src/main/java/com/biornaquantics/labreportloader/Test.java
+++ src/main/java/com/biornaquantics/labreportloader/Test.java
@@ -5,28 +5,10 @@
  */
 package com.biornaquantics.labreportloader;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-import java.io.IOException;
-import java.util.List;
-import org.bson.Document;
 
 public class Test {
     public static void main(String[] args){
-        /*MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-        MongoDatabase database = mongoClient.getDatabase("biornaquantics");
-        MongoCollection<Document> collection = database.getCollection("people");
-        MongoCursor<Document> cursor = collection.find().iterator();
-        try {
-            while (cursor.hasNext()) {
-                System.out.println(cursor.next().toJson());
-            }
-        } finally {
-            cursor.close();
-        }*/
-        System.out.println("{\"LabName\":\"citramic\",\"InternalName\":\"LaLA\"}");
+        
      
     }
 }
