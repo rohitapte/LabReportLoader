0194b2d1fe677068b6802f0da154ec54ca5ac67b
Rohit Apte <rohitapte@gmail.com>
2018-09-18 20:32:06 +0800

    empty repo

--- src/main/java/com/biornaquantics/labreportloader/Test.java
+++ src/main/java/com/biornaquantics/labreportloader/Test.java
@@ -5,42 +5,19 @@
  */
 package com.biornaquantics.labreportloader;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 
 public class Test {
     public static void main(String[] args){
-        Map<String,Object> parameters=new HashMap<>();
-        parameters.put("model","measurements");
-        Map<String,String> whereMap=new HashMap<>();
-        List<Object> measurementsList=new ArrayList<>();
-        Map<String,String> measurement=new HashMap<>();
-        measurement.put("key","4-hydroxyphenyllactic-acid-urine");
-        measurement.put("value" ,"0.35");
-        measurement.put("measuredAt","9/4/18");
-        measurementsList.add(measurement);
-        measurement=new HashMap<>();
-        measurement.put("key","4-hydroxybutyric-acid-urine");
-        measurement.put("value" ,"0.32");
-        measurement.put("measuredAt","9/4/18");
-        measurementsList.add(measurement);
-        measurement=new HashMap<>();
-        measurement.put("key","4-hydroxybutyric-acid-urine");
-        measurement.put("value" ,"1.7");
-        measurement.put("measuredAt","9/4/18");
-        measurementsList.add(measurement);
-        parameters.put("importRows",measurementsList);
-        whereMap.put("author","5add2f328ed48dc879934eba");
-        whereMap.put("panelId","5b9e5bb12d1eb83c28c18669");
-        whereMap.put("status","pending");
-        parameters.put("where",whereMap);
-        JSONObject jsonObject=new JSONObject(parameters);
-        JSONObject returnObject=new JSONObject();
-        String jsonFormattedMap=jsonObject.toString();
-        System.out.println(jsonFormattedMap);
+        
     }
 }
